8fa868991e49d4baa82beca9be8f95b0e43e00af
cauecarvalho255 <cauecarvalho255@gmail.com>
2026-03-28 07:21:48 -0400

    Add program to calculate squares of array elements

---
+++ lista_3/exercicio02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    
+    float vetor[10];
+    float vetorR[10];
+    int i;
+    
+    printf("Insira os elementos do vetor:\n");
+    
+    for(i = 0; i < 10; i++){
+        printf("[%i]: ", i);
+        scanf("%f", &vetor[i]);
+    }
+
+    printf("O quadrado dos elementos do vetor eh:\n");
+    
+    for(i = 0; i < 10; i++){
+       vetorR[i] = vetor[i] * vetor[i];
+       printf("[%.2f] ", vetorR[i]);
+    }
+    
+    return 0;
+    
+}
